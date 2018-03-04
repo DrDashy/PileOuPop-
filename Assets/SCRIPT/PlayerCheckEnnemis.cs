1ab1561adf6190ffb41ff65eef87c1e80b9b78d3
DrInfect <drinfect@hotmail.fr>
2018-03-03 18:37:37 -0600

    Modification du son de l'ennmis quand il est proche + suppression de truc inutile + ennemis spawn alléatoire et dispawn apres un nombre de salle entrer

--- Assets/SCRIPT/PlayerCheckEnnemis.cs
+++ Assets/SCRIPT/PlayerCheckEnnemis.cs
@@ -4,43 +4,32 @@ using UnityEngine;
 
 public class PlayerCheckEnnemis : MonoBehaviour {
 
-    public GameObject Player;
-    public GameObject Ennemis;
-
-    [Header("Distance zones Calme : ")]
-    public float calmDistance;
-
-    [Header("Distance zones Enemy Proche : ")]
-    public float alertDistance;
+    private GameObject Player;
 
     [Header("Distance zones Danger : ")]
     public float dangerDistance;
 
     [Header("Volume max du son : ")]
+    [Range(0,1)]
     public float maxVolume;
 
     [Header("Volume min du son : ")]
+    [Range(0, 1)]
     public float minVolume;
 
     [Header("Vitesse de FadeIn et FadeOut du son : ")]
     public float speedVolume;
 
-    private bool inCalmZone = true;
-    private bool inAlertZone = false;
     private bool inDangerZone = false;
-
-    [Header("AudioSource Zone Calm: ")]
-    public AudioSource audioSourceCalm;
-
-    [Header("AudioSource Zone Alert : ")]
-    public AudioSource audioSourceAlert;
 
     [Header("AudioSource Zone Danger : ")]
     public AudioSource audioSourceDanger;
 
     // Use this for initialization
-    void Start () {
-        
+    void Awake () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        audioSourceDanger.Stop();
+        audioSourceDanger.volume = 0;
     }
 	
 	// Update is called once per frame
@@ -48,87 +37,37 @@ public class PlayerCheckEnnemis : MonoBehaviour {
         CheckDistanceEnnemis();
     }
 
-    private void FixedUpdate()
-    {
-        if (inCalmZone)
-        {
-            FadeOut(audioSourceAlert);
-            FadeOut(audioSourceDanger);
-
-            FadeIn(audioSourceCalm);
-        }
-
-        if (inAlertZone)
-        {
-            FadeOut(audioSourceCalm);
-            FadeOut(audioSourceDanger);
-
-            FadeIn(audioSourceAlert);
-        }
-
-        if (inDangerZone)
-        {
-            FadeOut(audioSourceCalm);
-            FadeOut(audioSourceAlert);
-
-            FadeIn(audioSourceDanger);
-        }
-    }
-
     private void CheckDistanceEnnemis()
     {
-        // Calme Zone
-        if (Vector3.Distance(Ennemis.transform.position, Player.transform.position) > calmDistance && !inCalmZone)
+        // Danger Zone
+        if (Vector3.Distance(this.transform.position, Player.transform.position) < dangerDistance && !inDangerZone)
         {
-            inCalmZone = true;
-            inAlertZone = false;
-            inDangerZone = false;
-
-            audioSourceCalm.Play();
-            audioSourceAlert.Stop();
-            audioSourceDanger.Stop();
-        }
-
-        // Alert Zone
-        if (Vector3.Distance(Ennemis.transform.position, Player.transform.position) <= calmDistance && Vector3.Distance(Ennemis.transform.position, Player.transform.position) > dangerDistance && !inAlertZone)
-        {
-            inCalmZone = false;
-            inAlertZone = true;
-            inDangerZone = false;
-
-            audioSourceCalm.Stop();
-            audioSourceAlert.Play();
-            audioSourceDanger.Stop();
-        }
-
-        // Alert Zone
-        if (Vector3.Distance(Ennemis.transform.position, Player.transform.position) < dangerDistance && !inDangerZone)
-        {
-            inCalmZone = false;
-            inAlertZone = false;
             inDangerZone = true;
 
-            audioSourceCalm.Stop();
-            audioSourceAlert.Stop();
             audioSourceDanger.Play();
+
+            StartCoroutine(IncrementVolume());
         }
     }
 
-    private void FadeIn(AudioSource audioSource)
+    IEnumerator IncrementVolume()
     {
-        if(audioSource.volume < maxVolume)
+        yield return new WaitForSeconds(1f);
+        if (audioSourceDanger.volume < maxVolume)
         {
-            audioSource.volume += speedVolume;
-            Invoke("FadeIn", 1f);
+            audioSourceDanger.volume += speedVolume;
+            StartCoroutine(IncrementVolume());
         }
     }
 
-    private void FadeOut(AudioSource audioSource)
+    IEnumerator DecrementVolume()
     {
-        if (audioSource.volume > 0)
+        yield return new WaitForSeconds(1f);
+        if(audioSourceDanger.volume > 0)
         {
-            audioSource.volume -= speedVolume;
-			Invoke ("FadeOut", 0.5f);
+            audioSourceDanger.volume -= speedVolume;
+            StartCoroutine(IncrementVolume());
         }
     }
+
 }
--- Assets/SCRIPT/RoomLoading/RoomLoader.cs
+++ Assets/SCRIPT/RoomLoading/RoomLoader.cs
@@ -35,6 +35,7 @@ public class RoomLoader : MonoBehaviour {
 
     public GameObject PreviousRoom;
 
+    public EnnemisManagerSpawn EnnemisManger;
 
     public bool isInTheRoom = false;
 
@@ -69,6 +70,7 @@ public class RoomLoader : MonoBehaviour {
 
     public void LoadRooms()
     {
+        EnnemisManger.conteurSalle++;
         if (!Loaded) {   
             switch ((int)transform.eulerAngles.y / 90 % 4)
             {
