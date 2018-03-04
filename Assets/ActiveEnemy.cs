bdb76806a4eca7a41765246d9c8e08ec3c2348de
DrInfect <drinfect@hotmail.fr>
2018-03-04 15:52:15 -0600

    Try Fin

---
+++ Assets/ActiveEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveEnemy : MonoBehaviour {
+
+    private GameObject EnnemyManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnnemyManager = GameObject.FindGameObjectWithTag("EnnemisManager");
+        EnnemyManager.SetActive(true);
+    }
+}
---
+++ Assets/ActiveFin.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ActiveFin : MonoBehaviour, IActivable {
+
+	public void Activate()
+    {
+        SceneManager.LoadScene("Fin");
+    }
+
+    public void Highlight()
+    {
+        
+    }
+}
