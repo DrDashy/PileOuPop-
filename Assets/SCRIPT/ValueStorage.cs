4fa5faae7d2b800e85cec2de7e807b7c7d748beb
Unknown <leo.posticblu@orange.fr>
2018-03-04 05:31:51 -0500

    Mise au propre des scripts

---
+++ Assets/SCRIPT/ValueStorage.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ValueStorage : MonoBehaviour {
+
+    private static ValueStorage instance;
+
+    public int respawn = 0;
+
+    private ValueStorage() { }
+
+    public static ValueStorage Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ValueStorage();
+            }
+            return instance;
+        }
+    }
+
+
+    //Mettre vos valeurs par défaut ici
+    public void ResetValues()
+    {
+
+    }
+
+    //Mettre ici les valeur des variable qui seront reset à la mort
+    public void ResetOnDeath()
+    {
+
+    }
+}
